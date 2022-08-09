ed3902634c3c66576521f6d4fa7da9f1e065d781
ParkJiHo <iks15174@daum.net>
2022-08-09 14:21:46 +0900

    feat : Comment 엔티티 추가

---
+++ bigdata/src/main/java/com/jiho/bigdata/domain/comment/Comment.java
@@ -0,0 +1,23 @@
+package com.jiho.bigdata.domain.comment;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.jiho.bigdata.domain.post.Post;
+
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "postId")
+    private Post  post;
+}
