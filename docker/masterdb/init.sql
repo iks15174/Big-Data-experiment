6b69f70899374d1d83b6817ccbf8eef81c9c2821
ParkJiHo <iks15174@daum.net>
2022-08-13 17:43:47 +0900

    chore: 공백 제거

--- docker/masterdb/init.sql
+++ docker/masterdb/init.sql
@@ -1,2 +1,3 @@
-CREATE USER 'repl'@'%' IDENTIFIED WITH 'mysql_native_password' BY '1234'; 
+-- slave 접속 유저 생성
+CREATE USER 'repl'@'%' IDENTIFIED WITH mysql_native_password BY '1234'; 
 GRANT REPLICATION SLAVE ON *.* TO 'repl'@'%';
